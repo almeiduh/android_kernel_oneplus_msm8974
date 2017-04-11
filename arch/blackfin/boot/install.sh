29e245219c24d028a3303c93b74d43ab386b2972
Diogo Almeida <diogo.mms.almeida@gmail.com>
2017-04-11 18:19:02 +0100

    Modified OnePlus One Kernel files to allow MacOS compliation
    MacOS uses different options for "stat".
    This change detects if build is being done on Darwin
    and runs the "stat" command accordingly
    
    Change-Id: I0e9221e0191b579fc6b05c4fc10418de05a8fc3b

--- arch/blackfin/boot/install.sh
+++ arch/blackfin/boot/install.sh
@@ -46,7 +46,7 @@ fi
 back_it_up() {
 	local file=$1
 	[ -f ${file} ] || return 0
-	local stamp=$(stat -c %Y ${file} 2>/dev/null)
+	local stamp=$(if [[ "`uname`" == "Darwin" ]]; then stat -f %m ${file} 2>/dev/null; else stat -c %Y ${file} 2>/dev/null; fi)
 	mv ${file} ${file}.${stamp:-old}
 }
 
